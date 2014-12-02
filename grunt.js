f0afafb380377f537a0fc7b412081c23ac4d7a52
Scott González <scott.gonzalez@gmail.com>
2014-12-02 16:00:41 -0500

    Build: Remove unused tasks

--- grunt.js
+++ grunt.js
@@ -1,24 +1,16 @@
 var rimraf = require( "rimraf" );
 
-/*jshint node:true */
 module.exports = function( grunt ) {
-"use strict";
 
 var entryFiles = grunt.file.expandFiles( "entries/*.xml" );
 
-grunt.loadNpmTasks( "grunt-wordpress" );
-grunt.loadNpmTasks( "grunt-jquery-content" );
 grunt.loadNpmTasks( "grunt-check-modules" );
+grunt.loadNpmTasks( "grunt-jquery-content" );
+grunt.loadNpmTasks( "grunt-wordpress" );
 
 grunt.initConfig({
-	lint: {
-		grunt: "grunt.js"
-	},
 	xmllint: {
 		all: [].concat( entryFiles, "categories.xml", "entries2html.xsl", "notes.xsl" )
-	},
-	xmltidy: {
-		all: [].concat( entryFiles, "categories.xml" )
 	},
 	"build-pages": {
 		all: grunt.file.expandFiles( "pages/**" )
@@ -38,9 +30,7 @@ grunt.registerTask( "clean", function() {
 	rimraf.sync( "dist" );
 });
 
-grunt.registerTask( "default", "build-wordpress" );
 grunt.registerTask( "build", "build-pages build-xml-entries build-xml-categories build-xml-full build-resources" );
-grunt.registerTask( "build-wordpress", "check-modules clean lint xmllint build" );
-grunt.registerTask( "tidy", "xmllint xmltidy" );
+grunt.registerTask( "build-wordpress", "check-modules clean xmllint build" );
 
 };
